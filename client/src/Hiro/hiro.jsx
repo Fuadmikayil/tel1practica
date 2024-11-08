498f1047d64ee372e1e2250fa8c18cdc43411f4f
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-11-08 18:58:24 +0400

    Hiro

--- client/src/Hiro/hiro.jsx
+++ client/src/Hiro/hiro.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const hiro = () => {
   return (
-  <>
-  <h1 className='text-white'>Hiro</h1>
-  </>
-  )
-}
+    <>
+      <section className="h-[80vh] bg-blue-500 rounded-bl-[150px] ">
+        <div className="flex ">
+          <h2>Create physical and virtual cards for your business</h2>
+          <p>
+            Our landing page template works on all devices, so you only have to
+            set it up once, and get beautiful results forever.
+          </p>
+          <div>
+            <button className="bg-blue-600 p-2 rounded-xl tracking-widest text-white ">
+              <h5> Get your card</h5>
+            </button>
+            <button className="bg-blue-600 p-2 rounded-xl tracking-widest text-white ">
+              <h5> Get your card</h5>
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default hiro
+export default hiro;
--- client/src/header/Header.jsx
+++ client/src/header/Header.jsx
@@ -1,15 +1,12 @@
-import React from "react";
-
+import React from "react"; 
+import logo from "../İmages/logo.png"
 const Header = () => {
   return (
     <>
-      <header className="flex justify-between px-10 py-4  shadow-lg shadow-slate-800 items-center">
-        <nav className=" flex  gap-5 list-none">
-          <li className="text-white cursor-pointer">Home</li>
-          <li className="text-white cursor-pointer">Page</li>
-        </nav>
-        <button className="bg-blue-600 p-2 rounded-lg tracking-widest text-white">
-          Login
+      <header className="flex justify-between px-40 py-2   shadow-lg shadow-slate-800 items-center">
+      <img src={logo} alt="logo"  className=" w-14" />
+        <button className="bg-blue-600 p-2 rounded-xl tracking-widest text-white ">
+        <h5> Get your card</h5>
         </button>
       </header>
     </>
@@ -17,3 +14,4 @@ const Header = () => {
 };
 
 export default Header;
+ 
