cab5c6e5252248cba69d68adf1eacd5032b96687
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-11-09 00:49:33 +0400

    card section end

--- client/src/Hiro/hiro.jsx
+++ client/src/Hiro/hiro.jsx
@@ -4,7 +4,7 @@ import bg from "../İmages/hero-illustration.svg";
 const hiro = () => {
   return (
     <>
-      <section className="h-[75vh] bg-gradient-to-r from-sky-500 to-indigo-500 rounded-bl-[150px] flex gap-20  relative -z-50  ">
+      <section className="h-[75vh] bg-gradient-to-r from-sky-500 to-indigo-500 rounded-bl-[150px] flex gap-20  relative pt-5 ">
         <div className="flex flex-col  w-[50%] p-32 gap-y-6">
           <h2 className="text-white text-5xl font-bold tracking-wider">
             Create physical and virtual cards for your business
@@ -13,18 +13,52 @@ const hiro = () => {
             Our landing page template works on all devices, so you only have to
             set it up once, and get beautiful results forever.
           </p>
-          <div className="flex gap-3">
+          <div className="flex gap-3 ">
             <button className="bg-slate-800 p-2 rounded-2xl tracking-widest text-white ">
               <h5 className="tracking-wider font-bold px-1"> Get your card</h5>
             </button>
-            <button className="bg-blue-600 p-2 rounded-2xl tracking-widest text-white  ">
-              <h5> Get your card</h5>
+            <button className="bg-blue-600 p-2 rounded-2xl tracking-widest text-white  cursor-pointer ">
+              <h5> Read documentation</h5>
             </button>
           </div>
         </div>
-        <div className="relative top-20  ">
-          <img src={img} alt="hiroİmg" className="w-[600px]  absolute z-10   "/>
-          <img src={bg} alt="bg"  className="   mix-blend-lighten  translate-y-[-18%]  translate-x-[-11%]  " />
+        <div className="relative   ">
+          <img
+            src={img}
+            alt="hiroİmg"
+            className="w-[600px]  -bottom-36 absolute z-10   "
+          />
+          <img
+            src={bg}
+            alt="bg"
+            className="   mix-blend-lighten  translate-y-[-5%]  translate-x-[-11%]  "
+          />
+        </div>
+      </section>
+      <section className="flex gap-4 px-32 mt-28">
+        <div className="flex flex-col gap-1">
+          <i  className='bx bxs-credit-card-alt text-white text-3xl' ></i>
+          <h4 className="text-white  text-2xl">Create custom cards</h4>
+          <p className="text-gray-400 ">
+            Create cards that work exactly as you configured them. Make
+            real-time decisions on charges and spendings.
+          </p>
+        </div>
+        <div className="flex flex-col gap-1">
+          <i  className='bx bxs-id-card text-white text-3xl' ></i>
+          <h4 className="text-white  text-2xl">Create custom cards</h4>
+          <p className="text-gray-400">
+            Create cards that work exactly as you configured them. Make
+            real-time decisions on charges and spendings.
+          </p>
+        </div>
+        <div className="flex flex-col gap-1">
+          <i  className='bx bx-credit-card text-white text-3xl' ></i>
+          <h4 className="text-white  text-2xl">Create custom cards</h4>
+          <p className="text-gray-400">
+            Create cards that work exactly as you configured them. Make
+            real-time decisions on charges and spendings.
+          </p>
         </div>
       </section>
     </>
--- client/src/header/Header.jsx
+++ client/src/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <header className="flex justify-between px-40 py-2   shadow-lg shadow-slate-800 items-center z-50">
       <img src={logo} alt="logo"  className=" w-14" />
         <button className="bg-blue-600 p-2 rounded-xl tracking-widest text-white cursor-pointer ">
-        <h5> Get your card</h5>
+        <h5 className="font-bold"> Get your card</h5>
         </button>
       </header>
     </>
